591291ea51bb0e6830da782dd332327c7ca1990c
Larissa Ramos <devlarissasr@gmail.com>
2023-01-27 19:17:32 +0100

    feat: creating schema prisma + cors

---
+++ backend/prisma/migrations/20230127175700_create_table_users/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,35 @@
 // Backend API RESTful
 
-import Fastify from 'fastify';
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client';
+import cors from '@fastify/cors';
 
 const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(cors)
 
 /**
  * Metodo HTTP: Get, Post, Put, Patch, Delete
  */
 
-app.get('/hello', () => {
-  return "Hello World!";
+app.get('/hello', async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: 'b',
+      }
+    },
+  });
+
+  const user = await prisma.user.findMany({
+    where: {
+      name: {
+        startsWith: 'a',
+      }
+    },
+  });
+  return ([habits, user]);
 });
 
 app
